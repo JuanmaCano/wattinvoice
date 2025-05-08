6ff16e7be76c368f1aff85099a77c6c8b66f3a74
Juanma Cano <juanmacano@gmail.com>
2025-05-08 21:25:35 +0200

    Centrar contenido del componente App.jsx vertical y horizontalmente

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="text-4xl text-center text-gray-600">wattinvoice</h1>
+    </div>
+  );
+}
+
+export default App;
