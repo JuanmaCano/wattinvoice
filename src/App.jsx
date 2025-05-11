e0162ac7b3c9852f6ee633f6111e24effd807a8b
Juanma Cano <juanmacano@gmail.com>
2025-05-11 21:25:12 +0200

    clerk

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,19 @@
-import EnergyBill from "./EnergyBill.jsx";
-import consumo from "./data/consumo.json";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
 
-const billData = [
-  { label: "Monedero acumulado", value: "0,00 €" },
-  { label: "potencia contratada", value: "23,97 €" },
-  { label: "Bono social", value: "0,40 €" },
-  { label: "Alquiler x días", value: "0,83 €" },
-  { label: "Impuesto eléctrico", value: "1,86 €" },
-  { label: "De la red kWh", value: "247,764", highlight: "positive" },
-  { label: "", value: "31,42 €" },
-  { label: "Excedentes kWh", value: "195,185", highlight: "negative" },
-  { label: "", value: "-19,52 €" },
-  { label: "Acumulados en esta factura", value: "0,00 €" },
-  { label: "Base Imponible", value: "38,96 €" },
-  { label: "Iva 21%", value: "8,18 €" },
-  { label: "Total", value: "47,14 €", highlight: "positive" },
-  { label: "Monedero", value: "0" },
-  { label: "TOTAL A PAGAR", value: "47,14 €", highlight: "positive" },
-];
-
-console.log(consumo);
-
-function App() {
-  return <EnergyBill data={billData} month="enero 2025" />;
+export default function App() {
+  return (
+    <header>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
+  );
 }
-
-export default App;
